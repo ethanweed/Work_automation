b4e5fbf204016044a7440a3145b8c05afd0fe98b
ethanweed <ethan@Ethans-MacBook-Pro.local>
2015-03-22 08:50:08 -0400

    2015-22-03
    extracts email addresses from a text file and puts them in the
    clipboard for pasting

---
+++ extract_emails.py
@@ -0,0 +1,26 @@
+# make email list
+# extracts email addresses from a textfile and puts them in the clipboard for pasting
+
+# the list of emails needs to be in a textfile called "emails" located on desktop
+f = '/Users/ethan/Desktop/emails.txt'
+f_ile = open(f)
+f_txt = f_ile.read()
+
+# use regex module to find email addresses
+# this snippet lifted from: http://stackoverflow.com/questions/17681670/extract-email-sub-strings-from-large-document
+import re
+match = re.findall(r'[\w\.-]+@[\w\.-]+', f_txt)
+
+# put the emails in a string that can be copied and pasted
+output = ", ".join(match)
+
+#output data to the clipboard
+import subprocess
+
+def setClipboardData(data):
+	p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
+	p.stdin.write(data)
+	p.stdin.close()
+ 	retcode = p.wait()
+
+setClipboardData(output)
