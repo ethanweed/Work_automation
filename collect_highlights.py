9038185a77f41378d2064d445811f37d21db87b5
ethanweed <weed.ethan@gmail.com>
2022-09-01 11:33:34 +0200

    Update collect_highlights.py
    added comments

--- collect_highlights.py
+++ collect_highlights.py
@@ -6,10 +6,18 @@
 
 import os
 path = '/path/to/my/pdfs/'
+
+# get a list of all the markdown files in the folder
 files = [file for file in os.listdir(path) if file.endswith('.md')]
+
+# if the script has been run before, delete the old collection of highlights and notes, so that I don't just append to it.
 if '_highlights.md' in files:
 	os.remove ('_highlights.md') 
+
+# now that the old collection has been removed, rebuild the list of markdown files
 files = [file for file in os.listdir(path) if file.endswith('.md')]
+
+# grab the contents of each file, and throw it into the new "_highlights.md" file
 for file in files:
     f = open(file)
     text = f.read()
